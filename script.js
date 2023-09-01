1a2282ac0ebacedfc0782c1f8c678dd6ed27a55f
Sophie Gamer <sophiergamer@gmail.com>
2023-09-01 11:03:05 -0400

    rearranged order and clarified some lines

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const h2 = document.createElement('h2"')
 h2.textContent = 'This content added by JavaScript'
 document.querySelector('body').appendChild('h2')
 
-const showMe = document.getElementById('clickEvent')
-showMe.addEventListener('click', reveal)
-function reveal (){isRevealed.style.visibility = 'visible'}
-document.getElementById('struggles').style.visibility = 'hidden'
 const isRevealed = getElementById('struggles')
+isRevealed.style.visibility = 'hidden'
+const showMe = document.getElementById('clickEvent')
+
+function reveal (){isRevealed.style.visibility = 'visible'}
+showMe.addEventListener('click', reveal)
