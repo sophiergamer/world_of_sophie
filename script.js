43bab9117209749c7d81899be647d6345b0cbf15
Sophie Gamer <sophiergamer@gmail.com>
2023-09-01 10:44:41 -0400

    first attempt at coding clickEvent

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement('h2"')
 h2.textContent = 'This content added by JavaScript'
 document.querySelector('body').appendChild('h2')
+
+const showMe = document.getElementById('clickEvent')
+showMe.addEventListener('click', reveal)
+function reveal (){console.log(document.getElementById('struggles'))}
